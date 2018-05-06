a0a47a2677b6593a7d4cfa15165f62be2004bd78
pi3rrot <4dakto@gmail.com>
2018-05-06 23:40:36 +0900

    portal feature

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Basic React 16
+
+### Return Types
+
+```javascript
+
+// Before
+render() {
+  return (
+    <div> // 쓸모없는 warpper
+      <header></header>    
+      <div></div>
+      <footer></footer>
+    </div> 
+  );
+}
+
+// Before
+render() {
+  return (
+    <Fragment>
+      <header></header>
+      <div></div>
+      <footer></footer>
+    </Fragment>
+  );
+}
+  
+ ```
+ 
+ - string을 컴포넌트 리턴타입으로 사용할 수 있음
--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { createPortal } from 'react-dom';
+
+class Portals extends Component {
+  render() {
+    return createPortal(
+      <Message />,
+      document.getElementById('touchme')
+    )
+  }
+}
+
+const Message = () => 'Just touched it!';
+
+class ReturnTypes extends Component{
+  render() {
+    return 'hello';
+  }
+}
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <Fragment>
+        <ReturnTypes />
+        <Portals />
+      </Fragment>
+    );
   }
 }
 
